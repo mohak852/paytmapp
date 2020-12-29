e48a96bc21003bffe1eb28e135b4565d23146198
mohak852 <ajayshri2011@gmail.com>
2020-12-30 01:04:09 +0530

    first commit

---
+++ paytm.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, render_template
+from werkzeug.wrappers import Response
+from Checksum import generate_checksum
+app = Flask(__name__)
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/checkout')
+def checkout():
+    MERCHANT_KEY = 'f8tI!05cG49PjgW6'
+    param_dict = {
+        'MID': 'fSduEn46698831465981',
+        'ORDER_ID': str("868464"),
+        'TXN_AMOUNT': str("565165"),
+        'CUST_ID': "abc@abc.com",
+        'INDUSTRY_TYPE_ID': 'WEBSTAGING',
+        'WEBSITE': 'DEFAULT',
+        'CHANNEL_ID': 'WAP',
+    }
+    param_dict['CHECKSUMHASH'] = generate_checksum(
+        param_dict, MERCHANT_KEY)
+    print(param_dict['CHECKSUMHASH'])
+    return jsonify({'checksum': param_dict['CHECKSUMHASH']})
+
+if __name__ == '__main__':
+    app.run(debug=True)
